ff3ac8200d28504b3c07711ed2c6b48e048dcf41
Jay Kint <jaykint@microsoft.com>
2012-07-17 14:18:19 -0700

    SQL Server Bit columns returned as Javascript boolean values.
    Metadata updated to return 'boolean' as type when retrieving bit
    column.
    
    Test added.
    
    Fixes a portion if issue #4.
    
    Microsoft Internal Review Id: jaykint-6c2184b1b6dc497ca9b16ab92d3d47d8

--- src/ResultSet.cpp
+++ src/ResultSet.cpp
@@ -43,8 +43,10 @@ namespace mssql
             case SQL_WLONGVARCHAR:
                 typeName = L"text";
                 break;
-            case SQL_SMALLINT:
             case SQL_BIT:
+                typeName = L"boolean";
+                break;
+            case SQL_SMALLINT:
             case SQL_TINYINT:
             case SQL_INTEGER:
             case SQL_DECIMAL:
